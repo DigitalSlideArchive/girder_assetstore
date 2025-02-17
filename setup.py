d1986bba736f2baf2ef9a5db31c88d4dacd91ddc
willdunklin <will.dunklin@kitware.com>
2025-02-17 11:45:50 -0500

    Bump plugin version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setup(
     name='girder_assetstore',
-    version='0.1.0',
+    version='0.2.0',
     description='A Girder plugin to connect remote Girder instances as Assetstores.',
     long_description=readme,
     long_description_content_type='text/markdown',
